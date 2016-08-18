80f415e9d02f1379ea3e530bcced46d539272cc2
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2016-08-19 04:16:16 +0530

    setup git

--- tasks/setup.js
+++ tasks/setup.js
@@ -1,59 +1,66 @@
+const series = require('run-series');
 const directory = process.cwd();
 const git = require('simple-git')(directory);
-let chalk;
-
 git.silent(true);
 
-/* isRepo
- * Check if directory is a git repo
+let chalk;
+
+
+/* initRepo
+ * git init and add all the files!
  */
 
-let isRepo = (callback) => {
-    git.status((err, response) => {
-        if (err && err.includes('Not a git repository')) callback(false);
-        else callback(true);
-    });
-};
-
-let checkUnsavedChanges = (callback) => {
-    git.status((err, response) => {
-        if (response.modified.length) {
-            console.log(chalk.red(`
-                You have local changes in your repository.
-                Please, commit your changes or stash them before you can start learning.`));
-            callback(true);
-        } else callback(false);
-    });
-};
-
-/* add all the files! */
-
-let addAllFiles = (callback) => {
+let initRepo = (callback) => {
+    git.init();
     git.status((err, response) => {
         git.add(response.not_added, callback);
     });
 };
 
+/* Setup repo
+ *
+ * Check if directory is a repo
+ * If it isn't, call init Repo
+ */
+
+let setupRepo = (callback) => {
+    git.status((err, response) => {
+        if (err && err.includes('Not a git repository')) initRepo(callback);
+        else callback();
+    });
+};
+
+/* handleUnsavedChanges
+ *
+ * If there are unsaved changes, quit.
+ */
+
+let handleUnsavedChanges = (callback) => {
+    git.status((err, response) => {
+        console.log(response);
+        if (response.modified.length) {
+            console.log(chalk.red(`
+                You have local changes in your repository.
+                Please, commit your changes or stash them before you can start learning.`));
+            process.exit();
+        } else callback();
+    });
+};
+
+/* Setup complete */
+
+let setupComplete = () => {
+    console.log('setup complete');
+};
+
 /* Setup
+ *
  * Get things ready for lessons
  */
 
 let setup = (vorpal) => {
     chalk = vorpal.chalk;
-    isRepo(repo => {
-        if (!repo) {
-            git.init();
-            addAllFiles(() => {
-                console.log(chalk.green('Initialized git repo'))
-                console.log('all clear');
-            });
-        } else {
-            checkUnsavedChanges(unsavedChanges => {
-                if (unsavedChanges) process.exit(1);
-                else console.log('all clear');
-            });
-        }
-    });
+    series([setupRepo, handleUnsavedChanges, setupComplete]);
 };
 
 module.exports = setup;
