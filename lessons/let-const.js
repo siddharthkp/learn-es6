789c4ac7e8365e98fc549ff935ec6485379695a5
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2016-09-09 21:41:08 +0530

    Trying to bring some structure to the lesson

--- lessons/let-const.js
+++ lessons/let-const.js
@@ -1,18 +1,23 @@
 const cardinal = require('cardinal');
 const chalk = require('chalk');
 
-module.exports = {
-    gyan: [
-chalk.green(`
-Block Scope (let keyword)
-`),
-`
+let lesson = {
+    title: 'Block Scope (let keyword)'
+};
+
+lesson.gyan = [
+    {
+        type: 'text',
+        value: `
 Up until ES6, variables (var) are always function scoped.
 
 It doesn't matter where you define your variable -- the variable is available anywhere within the function.
 This allows you to write code that you wouldn't expect to work.
-`,
-cardinal.highlight(`
+        `
+    },
+    {
+        type: 'code',
+        value: `
     function fnScoped() {
         if(true) {
             var zed = 'alive'; // zed is "hoisted" to the function block
@@ -22,13 +27,22 @@ cardinal.highlight(`
             console.log('zed is ' + zed); // zed is visible
         }
     }
-`),
+        `
 
-`
+    },
+    {
+        type: 'break'
+    },
+    {
+        type: 'text',
+        value: `
 In ES6, the let keyword allows you to define variables within the scope of the block (block scoping).
 This makes variables behave more like what you might expect.
-`,
-cardinal.highlight(`
+        `
+    },
+    {
+        type: 'code',
+        value: `
     function blockScoped() {
         if(true) {
             let zed = 'alive'; // zed is not "hoisted" out of this block
@@ -36,16 +50,30 @@ cardinal.highlight(`
 
         console.log('zed is ' + zed); // zed is not visible
     }
-`),
-`
+        `
+    },
+    {
+        type: 'break'
+    },
+    {
+        type: 'text',
+        value: `
 In addition to the let keyword which is block scoped, there is another new way to declare values in ES6: const.
 Like most languages, const values cannot be changed at runtime. Any code which trys to do this will fail when the code is evaluated.
-`,
-cardinal.highlight(`
+        `
+    },
+    {
+        type: 'code',
+        value: `
     const foo = "bar";
     console.log(foo);
 
     foo = "baz"; // evalutation error
-`)
-    ]
-};
+        `
+    },
+    {
+        type: 'break'
+    }
+];
+
+module.exports = lesson;
