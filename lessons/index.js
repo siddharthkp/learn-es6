1468677d3253475cb5df5907f86ea4140bff0034
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2016-09-10 02:26:37 +0530

    Resume lesson

--- lessons/index.js
+++ lessons/index.js
@@ -1,4 +1,5 @@
 const clear = require('clear');
+const fs = require('fs');
 const helpers = require('../helpers');
 const chalk = require('chalk');
 
@@ -22,6 +23,19 @@ let lessonList = [
 let lessons = [];
 for (let name of lessonList) lessons.push (require('./' + name));
 
+let index = 0;
+
+let updateIndex = (index) => {
+    try {
+        let response = fs.writeFileSync(__dirname + '/learn-index.tmp', index);
+    } catch (err) {
+        console.log(chalk.yellow(`
+            Could not create temp file.
+            Your progress will not be saved
+        `));
+    }
+};
+
 let transform = (lesson) => {
     clear();
     console.log(chalk.green(lesson.title));
@@ -40,19 +54,30 @@ let transform = (lesson) => {
     console.log(`Done! Use ${chalk.yellow('git diff')} to see the changes`);
     console.log(`You can commit these changes or you can stash/checkout them.`);
     console.log();
+    updateIndex(++index);
     console.log(`You can continue your lesson by typing ${chalk.yellow('learn')}`);
     console.log();
 };
 
-let index = 0;
+let getIndex = () => {
+    try {
+        let response = fs.readFileSync(__dirname + '/learn-index.tmp', 'utf8');
+        return response;
+    } catch (err) {
+        if (err.code === 'ENOENT') updateIndex(0);
+        return 0;
+    }
+};
+
 let start = () => {
+    index = getIndex();
     let lesson = lessons[index];
     clear();
     helpers.render(lesson);
     helpers.pause();
     if (lesson.transform) transform(lesson);
     else {
-        index++;
+        updateIndex(++index);
         start();
     }
 };
